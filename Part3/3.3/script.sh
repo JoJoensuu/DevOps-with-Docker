b05899b60622b58254f762ae7e83fdb82348d885
Joni Joensuu <joni.joens@gmail.com>
2023-05-27 19:19:46 +0300

    3.3 - removed unnecessary lines

--- Part3/3.3/script.sh
+++ Part3/3.3/script.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 
-# Check if the right number of arguments are passed
-if [ "$#" -ne 2 ]; then
-    echo "Usage: ./builder.sh github_repo docker_repo"
-    exit 1
-fi
-
-# Set variables
 GITHUB_REPO=$1
 DOCKER_REPO=$2
 
-# Clone the GitHub repository
 git clone https://github.com/$GITHUB_REPO.git
 REPO_DIR=$(basename $GITHUB_REPO)
 
-# Navigate to the cloned repository
 cd $REPO_DIR
 
-# Build the Docker image
 docker build -t $DOCKER_REPO .
 
-# Push to Docker Hub
-# Docker login command can be added here, if needed
 docker push $DOCKER_REPO
 
-# Navigate back and remove the cloned repository
 cd ..
 rm -rf $REPO_DIR
-
-echo "Done."
